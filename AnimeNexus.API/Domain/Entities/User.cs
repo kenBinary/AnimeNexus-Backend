fc988ff5d3d69056721312a6e6f4e95a9469f737
kenBinary <kennethbecaro0@gmail.com>
2025-04-13 03:57:04 +0800

    feat: add unique constraint for UserName

--- AnimeNexus.API/Domain/Entities/User.cs
+++ AnimeNexus.API/Domain/Entities/User.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.AnimeNexus.API.Domain.Entities
@@ -6,6 +7,7 @@ namespace backend.AnimeNexus.API.Domain.Entities
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public required Guid UserId { get; set; }
+        [MaxLength(9)]
         public required string UserName { get; set; }
         public required string PasswordHash { get; set; }
     }
--- AnimeNexus.API/Infrastructure/Data/ApplicationDbContext.cs
+++ AnimeNexus.API/Infrastructure/Data/ApplicationDbContext.cs
@@ -11,5 +11,16 @@ namespace backend.AnimeNexus.API.Infrastructure.Data
         }
 
         public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                // tells EF Core to create a unique constraint in the database
+                entity.HasIndex(u => u.UserName)
+                      .IsUnique();
+            });
+        }
     }
 }
