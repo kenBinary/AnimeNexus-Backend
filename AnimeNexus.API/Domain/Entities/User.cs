ce53f6c114fa05eb232f26a07cb7bd9f1de0c0bd
kenBinary <kennethbecaro0@gmail.com>
2025-04-13 04:18:29 +0800

    fix: remove 'required' modifier from UserId property in User class

--- AnimeNexus.API/Domain/Entities/User.cs
+++ AnimeNexus.API/Domain/Entities/User.cs
@@ -6,7 +6,7 @@ namespace backend.AnimeNexus.API.Domain.Entities
     public class User
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public required Guid UserId { get; set; }
+        public Guid UserId { get; set; }
         [MaxLength(9)]
         public required string UserName { get; set; }
         public required string PasswordHash { get; set; }
