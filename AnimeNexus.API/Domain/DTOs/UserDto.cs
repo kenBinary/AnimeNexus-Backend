ffaffc60367e4ffb28ea8aaacd8b8d04b67db9da
kenBinary <kennethbecaro0@gmail.com>
2025-04-14 00:21:28 +0800

    feat: create UserDto class for user data transfer

---
+++ AnimeNexus.API/Domain/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.AnimeNexus.API.Domain.DTOs
+{
+    public class UserDto
+    {
+        public required string UserName { get; set; }
+    }
+}
