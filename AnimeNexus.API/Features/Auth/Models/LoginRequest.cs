b9a3f810e408ceaa58296489f9f1a49b97046899
kenBinary <kennethbecaro0@gmail.com>
2025-04-14 00:06:58 +0800

    feat: enhance LoginRequest model with validation attributes

--- AnimeNexus.API/Features/Auth/Models/LoginRequest.cs
+++ AnimeNexus.API/Features/Auth/Models/LoginRequest.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace backend.AnimeNexus.API.Features.Auth.Models
 {
     public class LoginRequest
     {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
         public required string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters.")]
         public required string Password { get; set; }
     }
 }
